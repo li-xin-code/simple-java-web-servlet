a003f5f8dc5e59bb1b7a75a5cba4171537c0674c
lixin <1605670114@qq.com>
2022-04-07 17:12:01 +0800

    LoginServlet add comments.

--- src/com/lixin/servlet/LoginServlet.java
+++ src/com/lixin/servlet/LoginServlet.java
@@ -4,17 +4,19 @@ import com.lixin.model.entity.User;
 import com.lixin.service.LoginService;
 import com.lixin.service.impl.LoginServiceImpl;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
+/**
+ * @author lixin
+ */
 @WebServlet(name = "loginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
+    private static final long serialVersionUID = -5167442880274699389L;
     private final LoginService loginService;
 
     public LoginServlet() {

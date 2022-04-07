caf7531f0d017901ae6ecaab6855b16710c4b613
lx <1605670114@qq.com>
2022-04-07 14:21:41 +0800

    ViewServlet add comments.

--- src/com/lixin/servlet/ViewServlet.java
+++ src/com/lixin/servlet/ViewServlet.java
@@ -1,6 +1,5 @@
 package com.lixin.servlet;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;

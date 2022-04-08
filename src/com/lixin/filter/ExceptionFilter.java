fbf76b511f36b9bf6ff44215d4f8f8207540defa
lixin <1605670114@qq.com>
2022-04-09 00:02:26 +0800

    add ExceptionFilter & ErrorHandlerServlet, error page config, add JsonUtils

---
+++ src/com/lixin/filter/ExceptionFilter.java
@@ -0,0 +1,39 @@
+package com.lixin.filter;
+
+import com.lixin.common.utils.JsonUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author lixin
+ */
+@WebFilter(filterName = "exceptionFilter", urlPatterns = "/*")
+public class ExceptionFilter extends GenericFilter {
+
+    private static final int ERROR = 500;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain)
+            throws IOException {
+        try {
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            PrintWriter writer = response.getWriter();
+            writer.flush();
+            JsonUtils.JsonObject result = JsonUtils.getJsonObj();
+            result.add("status", ERROR);
+            result.add("message", "request error");
+            result.add("data", e.getMessage());
+            System.out.println("exceptionFilter => " + result.toJsonString());
+            writer.println(result.toJsonString());
+            response.setContentType("application/json");
+        }
+
+    }
+}
---
+++ src/com/lixin/servlet/ErrorHandlerServlet.java
@@ -0,0 +1,33 @@
+package com.lixin.servlet;
+
+import com.lixin.common.utils.JsonUtils;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author lixin
+ */
+@WebServlet(name = "errorHandlerServlet", urlPatterns = "/error")
+public class ErrorHandlerServlet extends HttpServlet {
+    private static final long serialVersionUID = -854417701604871930L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        int status = resp.getStatus();
+        PrintWriter writer = resp.getWriter();
+        writer.flush();
+        JsonUtils.JsonObject result = JsonUtils.getJsonObj();
+        result.add("status", status);
+        result.add("message", "request error");
+        System.out.println("/error =>" + result.toJsonString());
+        writer.println(result.toJsonString());
+        resp.setContentType("application/json");
+    }
+
+}

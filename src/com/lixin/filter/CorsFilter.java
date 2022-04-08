bbb12df56c8cb9547106c066dd6dfbdb7438f6c1
lixin <1605670114@qq.com>
2022-04-08 19:30:02 +0800

    add CorsFilter

---
+++ src/com/lixin/filter/CorsFilter.java
@@ -0,0 +1,38 @@
+package com.lixin.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Cross-Origin Resource Sharing Filter
+ * 跨域资源共享过滤器
+ *
+ * @author lixin
+ */
+@WebFilter(filterName = "corsFilter", urlPatterns = "/*")
+public class CorsFilter extends GenericFilter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        resp.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+        resp.setHeader("Access-Control-Allow-Methods", "*");
+        resp.setHeader("Access-Control-Max-Age", "3600");
+        resp.setHeader("Access-Control-Allow-Headers", "*");
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(req, resp);
+
+    }
+    
+}
